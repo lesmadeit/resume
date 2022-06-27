13ec4ca84f7a331d8a5d6b659f562ff7901fb363
Mwendwa <muriithiken0@gmail.com>
2022-06-27 12:57:09 +0300

    new

---
+++ resume_demo/resume_demo/context_processors.py
@@ -0,0 +1,9 @@
+from multiprocessing import context
+from django.contrib.auth.models import User
+
+def project_context(request):
+    context = {
+        'me': User.objects.first(),
+    }
+
+    return context
